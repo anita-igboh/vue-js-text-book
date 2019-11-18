61f6922c0d01301a90da3cb692828b3861f18ed9
anita-igboh <anitaigboh@gmail.com>
2019-11-17 23:20:12 -0800

    initial commit

---
+++ vuejs-practice/src/store/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {hello: 'Hello from the store',
+  appName: `Anita's App`
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
---
+++ vuejs-practice/src/views/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h1>{{ $store.state.hello }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hello',
+  data() {
+    return {
+      hi: 'Hello world'
+    }
+  },
+  mounted() {
+    console.log(this.$store.state.hello)
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color:blue;
+}
+</style>
